c96bc8da030492fc463585cadb86ae4521bb15b5
Toebzy <Tobiascarlsen@live.dk>
2022-09-28 19:53:57 +0200

    SP1

---
+++ SP1Main.java/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  Scanner scan = new Scanner(System.in);
+  int numb = scan.nextInt();
+
+  public static void main(String []args){
+
+  Decoder.intToNumeral(scan.nextInt());
+
+
+  }
+
+}
